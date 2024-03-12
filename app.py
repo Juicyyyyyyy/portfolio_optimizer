32ff7c4851c392720d9fc192734b302c20cd4662
Juicyyyyyyy <declan.lowee@gmail.com>
2024-03-12 03:17:22 +0100

    Replace customtkinter with tkinter

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-import tkinter
-import customtkinter
+import tkinter as tk
+from tkinter import ttk
 import importlib
 import os
 
-# Set appearance and color theme
-customtkinter.set_appearance_mode("System")  # Consider allowing users to toggle between light and dark mode
-customtkinter.set_default_color_theme("blue")
+import sv_ttk
 
-
-class PortfolioOptimizerApp(customtkinter.CTk):
+class PortfolioOptimizerApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -17,7 +14,7 @@ class PortfolioOptimizerApp(customtkinter.CTk):
         self.geometry("800x500")
 
         # Container for all the pages
-        container = customtkinter.CTkFrame(self)
+        container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -38,53 +35,9 @@ class PortfolioOptimizerApp(customtkinter.CTk):
 
         self.show_frame("HomePage")
 
-        menubar = tkinter.Menu(self)
-
-        sidebar = customtkinter.CTkFrame(self, width=200, bg_color="#333")
-        sidebar.pack(side="left", fill="y")
-
-        # Sidebar "Home" button
-        home_button = customtkinter.CTkButton(sidebar, text="Home",
-                                              fg_color="#555", hover_color="#666",
-                                              text_color="white",
-                                              command=lambda: self.show_frame("HomePage"))
-        home_button.pack(pady=10, fill="x")
-
-        # Display the menu
-        self.config(menu=menubar)
-
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
-
-    def get_home_page_tickers_list(self):
-        home_page = self.frames["HomePage"]
-        return home_page.tickers_list
-
-    def get_home_page_tickers_string(self):
-        home_page = self.frames["HomePage"]
-        return home_page.ticker_string
-
-    def get_home_page_tickers_df(self):
-        home_page = self.frames["HomePage"]
-        return home_page.tickers_df
-
-    def get_home_page_start_date(self):
-        home_page = self.frames["HomePage"]
-        return home_page.start_date.get_date()
-
-    def get_home_page_end_date(self):
-        home_page = self.frames["HomePage"]
-        return home_page.end_date.get_date()
-
-    def get_home_page_portfolio_value(self):
-        home_page = self.frames["HomePage"]
-        return home_page.portfolio_size
-
-    def get_home_page_portfolio_id(self):
-        home_page = self.frames["HomePage"]
-        return home_page.portfolio_id
-
 
 if __name__ == "__main__":
     app = PortfolioOptimizerApp()
--- views/HomePage.py
+++ views/HomePage.py
@@ -1,139 +1,101 @@
-from tkinter import messagebox
-
 from PortfolioOptimizer.GptBasedFunctions import GptBasedFunctions
 from PortfolioOptimizer.MarketDataProvider import MarketDataProvider
 
-from datetime import datetime, timedelta
-import customtkinter
+from collections import OrderedDict
+
+from PortfolioOptimizer.EfficientFrontierCalculator import EfficientFrontierCalculator
+from PortfolioOptimizer.GptBasedFunctions import GptBasedFunctions as gpt
+
+import matplotlib.pyplot as plt
+from xhtml2pdf import pisa
+
+import tkinter as tk
+from tkinter import messagebox, ttk
 from tkcalendar import DateEntry
+from datetime import datetime, timedelta
 import os
 
 
-class HomePage(customtkinter.CTkFrame):
+# Assuming the rest of your imports are correct and necessary for your application
+
+class HomePage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-
-        self.portfolio_id = None
-        self.ticker_string = None
-        self.start_date_tf = None
-        self.end_date_tf = None
-        self.tickers_list = None
-        self.tickers_df = None
-        self.portfolio_size = None
         self.controller = controller
-
-        self.ticker_data = None
         self.gpt = GptBasedFunctions()
 
-        # Divide the main frame into two columns
-        self.column1 = customtkinter.CTkFrame(self)
-        self.column2 = customtkinter.CTkFrame(self)
-
+        self.column1 = tk.Frame(self)
+        self.column2 = tk.Frame(self)
         self.column1.pack(side="left", fill="both", expand=True)
         self.column2.pack(side="right", fill="both", expand=True)
 
-        # Title Label with updated font in column 1
-        self.label_title = customtkinter.CTkLabel(self.column1, text="Tickers", font=("Roboto Medium", -16))
+        self.label_title = tk.Label(self.column1, text="Tickers", font=("Roboto Medium", 16))
         self.label_title.pack(pady=20)
 
-        # Radio Buttons for User Choice with modern design in column 1
-        self.user_choice = customtkinter.StringVar(value="manual")
-        self.rb_manual = customtkinter.CTkRadioButton(self.column1, text="Enter Tickers Manually", variable=self.user_choice,
-                                                      value="manual", command=self.update_ui)
+        self.user_choice = tk.StringVar(value="manual")
+        self.rb_manual = ttk.Radiobutton(self.column1, text="Enter Tickers Manually", variable=self.user_choice,
+                                         value="manual", command=self.update_ui)
         self.rb_manual.pack(pady=2)
-        self.rb_ai = customtkinter.CTkRadioButton(self.column1, text="Generate Tickers by AI", variable=self.user_choice,
-                                                  value="ai", command=self.update_ui)
-        self.rb_ai.pack(pady=2)
+        self.rb_ai = ttk.Radiobutton(self.column1, text="Generate Tickers by AI", variable=self.user_choice, value="ai",
+                                     command=self.update_ui)
+        self.rb_ai.pack(pady=10)
 
-        # Entry for financial tickers with modern placeholder in column 1
-        self.entry_tickers = customtkinter.CTkEntry(self.column1, width=200, placeholder_text="Enter tickers e.g., AAPL, GOOGL, AMZN")
-        self.entry_tickers.pack(pady=2)  # Adjust as necessary for initial visibility
+        self.entry_tickers = tk.Entry(self.column1)
+        self.entry_tickers.pack(pady=2)
 
-        # AI Generation Parameters (initially hidden) in column 1
-        self.ai_frame = customtkinter.CTkFrame(self.column1)
-        self.label_risk_tolerance = customtkinter.CTkLabel(self.ai_frame, text="Risk Tolerance (high, moderate, low):")
-        self.entry_risk_tolerance = customtkinter.CTkEntry(self.ai_frame, width=200, placeholder_text="high")
-        self.label_investment_area = customtkinter.CTkLabel(self.ai_frame, text="Investment Area (tech, commodities, ...):")
-        self.entry_investment_area = customtkinter.CTkEntry(self.ai_frame, width=200, placeholder_text="tech")
-        self.label_investment_timeframe = customtkinter.CTkLabel(self.ai_frame, text="Investment Timeframe (short term, long term):")
-        self.entry_investment_timeframe = customtkinter.CTkEntry(self.ai_frame, width=200, placeholder_text="long term")
-        self.generate_button = customtkinter.CTkButton(self.ai_frame, text="Generate", command=self.generate_tickers)
+        self.ai_frame = tk.Frame(self.column1)
+        self.label_risk_tolerance = tk.Label(self.ai_frame, text="Risk Tolerance (high, moderate, low):")
+        self.entry_risk_tolerance = tk.Entry(self.ai_frame)
+        self.label_investment_area = tk.Label(self.ai_frame, text="Investment Area (tech, commodities, ...):")
+        self.entry_investment_area = tk.Entry(self.ai_frame)
+        self.label_investment_timeframe = tk.Label(self.ai_frame, text="Investment Timeframe (short term, long term):")
+        self.entry_investment_timeframe = tk.Entry(self.ai_frame)
+        self.generate_button = tk.Button(self.ai_frame, text="Generate", command=self.generate_tickers)
+        self.ticker_display = tk.Label(self.ai_frame, text="Generated Tickers will appear here")
 
-        self.ticker_display = customtkinter.CTkLabel(self.ai_frame, text="Generated Tickers will appear here", wraplength=500)
-
-        self.label_risk_tolerance.pack(pady=(10, 0))  # Add padding on top and bottom
-        self.entry_risk_tolerance.pack(pady=(5, 0))  # pady(top_padding, bottom_padding)
+        self.label_risk_tolerance.pack(pady=(10, 0))
+        self.entry_risk_tolerance.pack(pady=(5, 0))
         self.label_investment_area.pack(pady=(10, 0))
         self.entry_investment_area.pack(pady=(5, 0))
         self.label_investment_timeframe.pack(pady=(10, 0))
         self.entry_investment_timeframe.pack(pady=(5, 0))
-        self.generate_button.pack(pady=(20, 10), padx=20)  # Increase padding around the button
-
-        # Packing the AI-specific widgets into the frame
-        self.label_risk_tolerance.pack()
-        self.entry_risk_tolerance.pack()
-        self.label_investment_area.pack()
-        self.entry_investment_area.pack()
-        self.label_investment_timeframe.pack()
-        self.entry_investment_timeframe.pack()
-        self.generate_button.pack()
+        self.generate_button.pack(pady=(20, 10), padx=20)
         self.ticker_display.pack()
 
-        self.label_title_2 = customtkinter.CTkLabel(self.column2, text="Parameters", font=("Roboto Medium", -16))
+        self.ai_frame.pack_forget()  # Hide AI frame initially
+
+        self.label_title_2 = tk.Label(self.column2, text="Parameters", font=("Roboto Medium", 16))
         self.label_title_2.pack(pady=20)
 
-        # Elements for column 2: Date Pickers and Financial Model Options
-        # Date Picker for Start Date in column 2
-
         ten_years_ago = datetime.now() - timedelta(days=365 * 10)
+        self.label_start_date = tk.Label(self.column2, text="Start Date:")
+        self.start_date = DateEntry(self.column2, width=18, borderwidth=2, year=ten_years_ago.year,
+                                    month=ten_years_ago.month, day=ten_years_ago.day, state="readonly")
+        self.label_end_date = tk.Label(self.column2, text="End Date:")
+        self.end_date = DateEntry(self.column2, width=18, borderwidth=2, state="readonly")
 
-        self.label_start_date = customtkinter.CTkLabel(self.column2, text="Start Date:")
         self.label_start_date.pack(pady=2)
-        self.start_date = DateEntry(self.column2, width=18, background='darkblue',
-                                    foreground='white', borderwidth=2, year=ten_years_ago.year,
-                                    month=ten_years_ago.month, day=ten_years_ago.day,
-                                    state="readonly")
         self.start_date.pack(pady=2)
-
-        # Date Picker for End Date in column 2
-        self.label_end_date = customtkinter.CTkLabel(self.column2, text="End Date:")
         self.label_end_date.pack(pady=2)
-        self.end_date = DateEntry(self.column2, width=18, background='darkblue', foreground='white', borderwidth=2,
-                                  state="readonly")
         self.end_date.pack(pady=2)
 
-        # Option Menu for Financial Model in column 2
-        self.label_fin_model = customtkinter.CTkLabel(self.column2, text="Choose Financial Model:")
-        self.label_fin_model.pack(pady=2)
-        self.models = ["EfficientFrontier", "BlackLitterman"]
-        self.optionmenu_fin_model = customtkinter.CTkOptionMenu(self.column2, values=self.models)
-        self.optionmenu_fin_model.pack(pady=2)
-
-        # Chose portfolio size
-        self.label_portfolio_size = customtkinter.CTkLabel(self.column2, text="Choose your portfolio size in $:")
+        self.label_portfolio_size = tk.Label(self.column2, text="Choose your portfolio size in $:")
+        self.portfolio_size_input = tk.Entry(self.column2)
         self.label_portfolio_size.pack(pady=2)
-        self.portfolio_size_input = customtkinter.CTkEntry(self.column2, width=100, placeholder_text="10000")
-        self.portfolio_size_input.pack(pady=2)  # Add this line to pack the portfolio_size_input
+        self.portfolio_size_input.pack(pady=2)
 
-
-        # Continue Button with enhanced style in column 2
-        self.button_continue = customtkinter.CTkButton(self.column2, text="Continue", command=self.on_continue)
+        self.button_continue = tk.Button(self.column2, text="Continue", command=self.on_continue)
         self.button_continue.pack(pady=20)
 
-        # Initially update UI based on default choice
         self.update_ui()
 
     def update_ui(self):
-        choice = self.user_choice.get()
-        if choice == "ai":
-            self.ai_frame.pack(pady=10)  # Show the AI frame
-            self.entry_tickers.pack_forget()  # Hide the manual entry field
+        if self.user_choice.get() == "ai":
+            self.ai_frame.pack(pady=10)
+            self.entry_tickers.pack_forget()
         else:
-            self.ai_frame.pack_forget()  # Hide the AI frame
-            self.entry_tickers.pack(pady=2)  # Show the manual entry field
-
-        # Ensure Continue button is always at the bottom of column 2
-        self.button_continue.pack(pady=20)
+            self.ai_frame.pack_forget()
+            self.entry_tickers.pack(pady=2)
 
     def generate_tickers(self):
         risk_tolerance = self.entry_risk_tolerance.get()
@@ -145,6 +107,135 @@ class HomePage(customtkinter.CTkFrame):
 
     def download_data(self, tickers, start_date, end_date, return_updated_tickers: bool):
         return MarketDataProvider().get_data(tickers=tickers, start_date=start_date, end_date=end_date, return_updated_tickers=return_updated_tickers)
+
+    def convert_html_to_pdf(self, html_string, pdf_path):
+        with open(pdf_path, "wb") as pdf_file:
+            pisa_status = pisa.CreatePDF(html_string, dest=pdf_file)
+
+        return not pisa_status.err
+
+    def analyze(self):
+
+        efficient_frontier_calculator = EfficientFrontierCalculator(self.tickers_df)
+
+        weights = efficient_frontier_calculator.calculate_efficient_frontier_weights()
+
+        filtered_weights = OrderedDict((key, value) for key, value in weights.items() if value != 0)
+        dollar_sizes = {key: value * self.portfolio_size for key, value in filtered_weights.items()}
+
+        sorted_weights = OrderedDict(sorted(filtered_weights.items(), key=lambda x: x[1], reverse=True))
+        sorted_dollar_sizes = OrderedDict(sorted(dollar_sizes.items(), key=lambda x: x[1], reverse=True))
+
+        self.sorted_tickers_list = list(sorted_weights.keys())
+        self.tickers_string = ' '.join(self.sorted_tickers_list)
+        self.tickers_string = self.tickers_string.replace(" ", ", ")
+
+        filtered_weights_string_percent = ", ".join(
+            [f"{key}: {value * 100:.2f}%" for key, value in sorted_weights.items()])
+        filtered_dollar_sizes_string = ", ".join([f"{key}: ${value:.2f}" for key, value in sorted_dollar_sizes.items()])
+
+        expected_return, volatility, sharpe_ratio = efficient_frontier_calculator.calculate_efficient_frontier_performance()
+
+        expected_return_percent, volatility_percent = expected_return * 100, volatility * 100
+
+        # Create pie chart based on weights
+        labels = sorted_dollar_sizes.keys()
+        sizes = list(sorted_dollar_sizes.values())
+
+        # Create a static Pie chart using Matplotlib
+        fig, ax = plt.subplots()
+        ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+
+        # Equal aspect ratio ensures that the pie is drawn as a circle.
+        ax.axis('equal')
+
+        # Save the static pie chart as an image
+        img_path = f"created_portfolios/{self.portfolio_id}/pie_chart.png"
+        plt.savefig(img_path)
+
+        ticker_reviews = gpt.generate_tickers_review(
+            self.tickers_string)  # review of each ticker in markdown html format
+
+        html_content = f"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Portfolio Analysis</title>
+            <style>
+                body {{
+                    font-family: 'Arial', sans-serif;
+                    line-height: 1.6;
+                    margin: 20px;
+                    max-width: 800px;
+                    margin-left: auto;
+                    margin-right: auto;
+                    background-color: #f8f9fa;
+                    color: #495057;
+                }}
+
+                h1 {{
+                    font-size: 24px;
+                    color: #007bff;
+                    margin-bottom: 20px;
+                }}
+
+                h2 {{
+                    font-size: 20px;
+                    color: #007bff;
+                    margin-bottom: 15px;
+                }}
+
+                img {{
+                    max-width: 100%;
+                    height: auto;
+                    margin-bottom: 20px;
+                }}
+
+                p {{
+                    font-size: 16px;
+                    margin-bottom: 10px;
+                    color: #000;
+                }}
+
+                strong {{
+                    font-weight: bold;
+                }}
+
+                #ticker-reviews{{
+                    font-size: 16px;
+                    margin-bottom: 10px;
+                    color: #000;
+                }}
+            </style>
+        </head>
+        <body>
+            <h1>Portfolio Analysis</h1>
+            <img src="{img_path}" alt="Portfolio Weights">
+            <h2>Weights Percent</h2>
+            <p><strong>{filtered_weights_string_percent}</strong></p>
+            <h2>Weights Raw</h2>
+            <p><strong>{filtered_dollar_sizes_string}</strong></p>
+            <h2>Expected Return</h2>
+            <p><strong>{round(expected_return_percent, 2)}% per Year</strong></p>
+            <h2>Volatility</h2>
+            <p><strong>{round(volatility_percent, 2)}% per Year</strong></p>
+            <h2>Sharpe Ratio</h2>
+            <p><strong>{round(sharpe_ratio, 4)}</strong></p>
+            <h2>Ticker Reviews</h2>
+            <p><strong id="ticker-reviews">{ticker_reviews}</strong></p>
+        </body>
+        </html>
+        """
+
+        # Convert HTML to PDF
+        pdf_path = f"created_portfolios/{self.portfolio_id}/analysis.pdf"
+        if self.convert_html_to_pdf(html_content, pdf_path):
+            print(f"PDF generated and saved at {pdf_path}")
+
+        # Display the saved PDF
+        os.system(f"start {pdf_path}")
 
     def on_continue(self):
         choice = self.user_choice.get()
@@ -185,18 +276,11 @@ class HomePage(customtkinter.CTkFrame):
 
         os.makedirs('created_portfolios/' + now_str)
 
-        # Proceed to the next frame
-        chosen_model = self.optionmenu_fin_model.get()
-        if chosen_model == "BlackLitterman":
-            frame = self.controller.frames[chosen_model + "Page"]
-            frame.set_tickers(self.ticker_data.split(', '))
-        self.controller.show_frame(chosen_model + "Page")
-
         self.tickers_df, self.tickers_list = self.download_data(self.tickers_list, self.start_date.get_date(), self.end_date.get_date(), return_updated_tickers=True)
 
-        print(self.ticker_string)
         self.ticker_string = ' '.join(self.tickers_list)
         self.ticker_string = self.ticker_string.replace(" ", ", ")
-        print(self.ticker_string)
+
+        self.analyze()
 
 
